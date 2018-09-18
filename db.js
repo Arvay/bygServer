d915a0602e90007f8a9f7b917af73846110c8976
Arvay <PHONE:17600079797>
2018-09-18 14:44:49 +0800

    调试修改接口 -16

--- db.js
+++ db.js
@@ -29,9 +29,7 @@ app.get('/api/getlist', (req, res) => {
 //修改数据
 app.post('/api/updataUserInfo', (req, res) => {
     const sqlStr = 'UPDATE userInfo SET user_name = ?,user_phone = ? WHERE id = ?'
-    var modsqlparams = ['吕雪源l666e','26',5];
-    console.log('进来了')
-    conn.query(sqlStr,modsqlparams, (err,results) => {
+    conn.query(sqlStr,req.body, (err,results) => {
         if(err){
             console.log('err');
             return;
