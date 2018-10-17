e1ce464aa06498b7b68dfb9a450a46a8cc4fcc51
Arvay <17600079797>
2018-10-17 15:33:43 +0800

    排序

--- db.js
+++ db.js
@@ -33,7 +33,7 @@ app.get('/api/exportExcel', function(req, res, next) {
         caption:'string',
         type:'string',
         beforeCellWrite:function(row, cellData){
-            return cellData.toUpperCase();
+            return cellData
         },
         width:28.7109375
     },{
@@ -62,7 +62,12 @@ app.get('/api/exportExcel', function(req, res, next) {
         caption:'number',
         type:'number'
     }];
-    conf.rows = exselInfo;
+    conf.rows = [
+        ['pi', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
+        ["e", new Date(2012, 4, 1), false, 2.7182],
+        ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+        ["null date", null, true, 1.414]
+    ];
     var result = nodeExcel.execute(conf);
     res.setHeader('Content-Type', 'application/vnd.openxmlformats');
     res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
